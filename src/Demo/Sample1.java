5626d1ae61873f1b28c574b6db8654b6b829b1b7
shree-pc <shree-pc@vishal>
2022-09-12 18:51:27 +0530

    [Supriya]: created 2 classes as sample1 & sample2

---
+++ src/Demo/Sample1.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class Sample1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		
+		System.out.println("How are yuo?");
+	}
+
+}
---
+++ src/Demo/Sample2.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Sample2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi");
+		
+		System.out.println("I'm Fine. Thank yuo");
+	}
+}
